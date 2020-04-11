799e1781226491bc62523640436164dd861280ab
yuhongzhou <yuhongzhou@deepexi.com>
2020-04-11 17:45:59 +0800

    项目初始化

---
+++ src/main/java/com/shadow/technology_integration/TechnologyIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.shadow.technology_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shadow/technology_integration/TechnologyIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.technology_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
